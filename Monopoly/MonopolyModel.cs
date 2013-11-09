50ddf209404eebc5c725af9bab56a57974850e41
Ilja Fiers <ilja.fiers@chello.nl>
2013-11-09 15:21:52 +0100

    Klasse MonopolyModel toegevoegd.

---
+++ Monopoly/MonopolyModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly
+{
+    // Deze klasse is het 'model' uit het MVC design pattern.
+    // Het bevat alle 'state' binnen een monopolyspel,
+    // dat betekent alle gegevens, een soort van database voor het spel.
+    //
+    class MonopolyModel
+    {
+        private Dobbelsteen steen1;
+        private Dobbelsteen steen2;
+        private Vakjes bordVakjes;
+
+        public MonopolyModel()
+        {
+            steen1 = new Dobbelsteen();
+            steen2 = new Dobbelsteen();
+            bordVakjes = new Vakjes();
+        }
+    }
+}
