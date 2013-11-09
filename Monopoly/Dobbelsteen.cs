f2f2982cb6feba22e219c56fb5a8462f2926a5b2
Ilja Fiers <ilja.fiers@chello.nl>
2013-11-09 10:40:52 +0100

    Klasse 'Dobbelsteen' geimplementeerd en toegevoegd.

---
+++ Monopoly/Dobbelsteen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly
+{
+    // class Dobbelsteen
+    // Gebruik:
+    // - Maak een object van het type Dobbelsteen aan
+    // - Roep Gooi() aan voor een nieuwe waarde.
+    // - LaatsteWaarde retourneert de worp.
+    public class Dobbelsteen
+    {
+        Random rnd;
+        private int laatsteWaarde;
+
+        Dobbelsteen()
+        {
+            rnd = new Random();
+
+            // gooi 1x voor initialisatie, anders
+            // kan een gebruiker er eentje aanmaken en direct
+            // LaatsteWaarde aanspreken
+            Gooi();
+        }
+
+        public int Gooi()
+        {
+            laatsteWaarde = rnd.Next(1, 6);
+            return LaatsteWaarde;
+        }
+
+        public int LaatsteWaarde
+        {
+            get { return laatsteWaarde; }
+        }
+    }
+}
