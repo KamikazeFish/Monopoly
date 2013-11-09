05410948920278aa5e7b1038cf67085709b59c93
Ilja Fiers <ilja.fiers@chello.nl>
2013-11-09 15:44:18 +0100

    Vakje en vakjes verder uitgewerkt.

--- Monopoly/Vakje.cs
+++ Monopoly/Vakje.cs
@@ -49,19 +49,27 @@ namespace Monopoly
     //
     public class Vakje
     {
-        enum VakType
+        public enum VakType
         {
             START,
             STRAAT,
+            STATION,
             KANS,
-            ALGEMEENFONDS
+            ALGEMEENFONDS,
+            GEVANGENIS,
+            NAARGEVANGENIS,
+            BELASTING,
+            NUTSBEDRIJF,
+            VRIJPARKEREN
         }
 
+        private VakType tp;
         private string straatNaam;
         private string stadNaam;
 
-        public Vakje(string straatNaam, string stadNaam)
+        public Vakje(VakType tp, string straatNaam, string stadNaam)
         {
+            this.tp = tp;
             this.straatNaam = straatNaam;
             this.stadNaam = stadNaam;
         }
@@ -84,46 +92,46 @@ namespace Monopoly
         public Vakjes()
         {
             bord = new List<Vakje>();
-            Add(new Vakje( "Start",                 ""));
-            Add(new Vakje( "Dorpsstraat",           "Ons Dorp"));
-            Add(new Vakje( "Algemeen fonds",        ""));
-            Add(new Vakje( "Brink",                 "Ons Dorp"));
-            Add(new Vakje( "Inkomstenbelasting",    ""));
-            Add(new Vakje( "Station Zuid",          ""));
-            Add(new Vakje( "Steenstraat",            "Arnhem"));
-            Add(new Vakje( "Kans",                  ""));
-            Add(new Vakje( "Ketelstraat",           "Arnhem"));
-            Add(new Vakje( "Velperplein",           "Arnhem"));
-            Add(new Vakje( "Gevangenis",            ""));
-            Add(new Vakje( "Barteljorisstraat",     "Haarlem"));
-            Add(new Vakje( "Elektriciteitsbedrijf", ""));
-            Add(new Vakje( "Zijlweg",               "Haarlem"));
-            Add(new Vakje( "Houtstraat",            "Haarlem"));
-            Add(new Vakje( "Station West",          ""));
-            Add(new Vakje( "Neude",                 "Utrecht"));
-            Add(new Vakje( "Algemeen fonds",        ""));
-            Add(new Vakje( "Biltstraat",            "Utrecht"));
-            Add(new Vakje( "Vreeburg",              "Utrecht"));
-            Add(new Vakje( "Vrij parkeren",         ""));
-            Add(new Vakje( "A Kerkhof",             "Groningen"));
-            Add(new Vakje( "Kans",                  ""));
-            Add(new Vakje( "Grote Markt",           "Groningen"));
-            Add(new Vakje( "Heerestraat",           "Groningen"));
-            Add(new Vakje( "Station Noord",         ""));
-            Add(new Vakje( "Spui",                  "'s-Gravenhage"));
-            Add(new Vakje( "Plein",                 "'s-Gravenhage"));
-            Add(new Vakje( "Waterleiding",          ""));
-            Add(new Vakje( "Lange Poten",           "'s-Gravenhage"));
-            Add(new Vakje( "Naar de gevangenis",    ""));
-            Add(new Vakje( "Hofplein",              "Rotterdam"));
-            Add(new Vakje( "Blaak",                 "Rotterdam"));
-            Add(new Vakje( "Algemeen fonds",        ""));
-            Add(new Vakje( "Coolsingel",            "Rotterdam"));
-            Add(new Vakje( "Station Oost",          ""));
-            Add(new Vakje( "Kans",                  ""));
-            Add(new Vakje( "Leidschestraat",        "Amsterdam"));
-            Add(new Vakje( "Extra belasting",       ""));
-            Add(new Vakje( "Kalverstraat",          "Amsterdam"));
+            Add(new Vakje(Vakje.VakType.START,          "Start",                 ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Dorpsstraat",           "Ons Dorp"));
+            Add(new Vakje(Vakje.VakType.ALGEMEENFONDS,  "Algemeen fonds", ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Brink", "Ons Dorp"));
+            Add(new Vakje(Vakje.VakType.BELASTING,      "Inkomstenbelasting",    ""));
+            Add(new Vakje(Vakje.VakType.STATION,        "Station Zuid",          ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Steenstraat", "Arnhem"));
+            Add(new Vakje(Vakje.VakType.KANS,           "Kans",                  ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Ketelstraat", "Arnhem"));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Velperplein", "Arnhem"));
+            Add(new Vakje(Vakje.VakType.GEVANGENIS,     "Gevangenis",            ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Barteljorisstraat", "Haarlem"));
+            Add(new Vakje(Vakje.VakType.NUTSBEDRIJF,    "Elektriciteitsbedrijf", ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Zijlweg", "Haarlem"));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Houtstraat", "Haarlem"));
+            Add(new Vakje(Vakje.VakType.STATION,        "Station West", ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Neude", "Utrecht"));
+            Add(new Vakje(Vakje.VakType.ALGEMEENFONDS,  "Algemeen fonds",        ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Biltstraat", "Utrecht"));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Vreeburg", "Utrecht"));
+            Add(new Vakje(Vakje.VakType.VRIJPARKEREN,   "Vrij parkeren",         ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "A Kerkhof", "Groningen"));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Kans", ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Grote Markt", "Groningen"));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Heerestraat", "Groningen"));
+            Add(new Vakje(Vakje.VakType.STATION,        "Station Noord", ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Spui", "'s-Gravenhage"));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Plein", "'s-Gravenhage"));
+            Add(new Vakje(Vakje.VakType.NUTSBEDRIJF,    "Waterleiding",          ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Lange Poten", "'s-Gravenhage"));
+            Add(new Vakje(Vakje.VakType.NAARGEVANGENIS, "Naar de gevangenis",    ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Hofplein", "Rotterdam"));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Blaak", "Rotterdam"));
+            Add(new Vakje(Vakje.VakType.ALGEMEENFONDS,  "Algemeen fonds",        ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Coolsingel", "Rotterdam"));
+            Add(new Vakje(Vakje.VakType.STATION,        "Station Oost", ""));
+            Add(new Vakje(Vakje.VakType.KANS,           "Kans", ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Leidschestraat", "Amsterdam"));
+            Add(new Vakje(Vakje.VakType.BELASTING,      "Extra belasting",       ""));
+            Add(new Vakje(Vakje.VakType.STRAAT,         "Kalverstraat", "Amsterdam"));
         }
 
         private void Add(Vakje vk)
