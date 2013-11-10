632a6adf84c3e4d0b1dcd732d0421105363c9bb2
Ilja Fiers <ilja.fiers@chello.nl>
2013-11-10 11:40:30 +0100

    Voeg begin van klasse speler toe.

---
+++ Monopoly/Speler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly
+{
+    public class Speler
+    {
+        private string naam;        // hoe heet deze speler?
+        private int saldo;          // heveel geld heeft deze speler?
+
+        public Speler(string naam, int saldo)
+        {
+            this.naam = naam;
+            this.saldo = saldo;
+        }
+
+        public string Naam
+        {
+            get { return naam; }
+        }
+
+        public int Saldo
+        {
+            get { return saldo; }
+        }
+    }
+}
