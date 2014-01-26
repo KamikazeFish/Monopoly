864d8b001dbbf49750fa35ad6d09c20fb45584b2
Ilja Fiers <ilja.fiers@chello.nl>
2014-01-26 16:28:40 +0100

    Enkele opschoonacties, verwijderen ongebruikte code.

--- Monopoly/Actie.cs
+++ Monopoly/Actie.cs
@@ -173,12 +173,12 @@ namespace Monopoly
 
     public class GaNaarActie : Actie
     {
-        private string naam;
+        private string naamVakje;
         private bool geldVoorLangsStart;
 
         public GaNaarActie(string naam, bool geldVoorLangsStart)
         {
-            this.naam = naam;
+            this.naamVakje = naam;
             this.geldVoorLangsStart = geldVoorLangsStart;
         }
 
@@ -202,9 +202,9 @@ namespace Monopoly
                 notFound++;
                 if (notFound > model.Vakjes.GetAantalVakjes())
                 {
-                    throw new Exception("Vakje '" + naam + "' niet gevonden!");
+                    throw new Exception("Vakje '" + naamVakje + "' niet gevonden!");
                 }
-            } while (model.Vakjes[model.Spelers.HuidigeSpeler.Positie].StraatNaam != naam);
+            } while (model.Vakjes[model.Spelers.HuidigeSpeler.Positie].StraatNaam != naamVakje);
 
             // vanuit deze actie, voer de landingsactie voor het vakje uit.
             //
@@ -214,83 +214,4 @@ namespace Monopoly
         }
     }
 
-    //public class GooiActie : Actie
-    //{
-    //    public GooiActie(string tekst = "De volgende speler mag gooien")
-    //    {
-    //    }
-
-    //    public override void VoerUit(MonopolyModel model) 
-    //    {
-
-    //    }
-    //}
-
-    //public class BetaalOntvangPerHuisHotelActie : Actie
-    //{
-    //    public BetaalOntvangPerHuisHotelActie(string tekst) : base(tekst)
-    //    {
-    //    }
-
-    //    public override void VoerUit(MonopolyModel model) 
-    //    {
-
-    //    }
-    //}
-
-    //public class GaNaarActie : Actie
-    //{
-    //    public GaNaarActie(string tekst) : base(tekst)
-    //    {
-    //    }
-
-    //    public override void VoerUit(MonopolyModel model) 
-    //    {
-
-    //    }
-    //}
-
-    //public class VerlaatDeGevangenisActie : Actie
-    //{
-    //    public VerlaatDeGevangenisActie(string tekst)
-    //        : base(tekst)
-    //    {
-    //    }
-
-    //    public override void VoerUit(MonopolyModel model) 
-    //    {
-
-    //    }
-    //}
-
-    //public class KeuzeActie : Actie
-    //{
-    //    Actie actie1;
-    //    Actie actie2;
-    //    string vraag;
-
-    //    public KeuzeActie(string tekst, string vraag, Actie actie1, Actie actie2)
-    //        : base(tekst)
-    //    {
-    //        this.actie1 = actie1;
-    //        this.actie2 = actie2;
-    //        this.vraag = vraag;
-    //    }
-
-    //    public override void VoerUit(MonopolyModel model) 
-    //    {
-    //        // TODO: Custom dialog maken om meteen actie 1 of actie 2 te kunnen selecteren. (of andere buttons)
-    //        DialogResult result = MessageBox.Show(this.vraag, "Wat wilt u doen?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-    //        if (result == DialogResult.Yes)
-    //        {
-    //            this.actie1.VoerUit(model);
-    //        }
-    //        else //if( result == DialogResult.No) // Als de dialoogbox weggedrukt wordt altijd actie 2 uitvoeren
-    //        {
-    //            this.actie2.VoerUit(model);
-    //        }
-
-    //    }
-    //}
 }
